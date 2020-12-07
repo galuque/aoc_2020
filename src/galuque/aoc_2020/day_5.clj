d7cb4893412e50ecd267f7f25d4d0ba6d324d107
Gabriel Luque <gldidonna@gmail.com>
2020-12-07 00:12:35 -0300

    alternate solutoin day 5

--- src/galuque/aoc_2020/day_5.clj
+++ src/galuque/aoc_2020/day_5.clj
@@ -2,61 +2,34 @@
   (:require [clojure.java.io :as io]
             [clojure.set :as set]))
 
-
 (def sample-pass "FBFBBFFRLR")
 
 (def input
   (line-seq (io/reader (io/resource "day_5/input.txt"))))
 
+(def encoding {\F 0 \B 1 \L 0 \R 1})
 
-(defn parse-pass
-  [pass]
-  (let [[_ row col]
-        (re-find #"([F|B]{7})([R|L]{3})" pass)]
-    [row col]))
+(defn chars->num [chars]
+  (Long/parseLong (reduce str (map encoding chars)) 2))
 
-(defn middle
-  [beg end]
-  (int (Math/ceil (/ (- end beg) 2))))
-
-(defn decode
-  [code end lower-half upper-half]
-  (loop [beg 0
-         end end
-         code code]
-    (if (nil? code)
-      end
-      (recur
-       (if (= lower-half (first code))
-         beg
-         (+ beg (middle beg end)))
-       (if (= upper-half (first code))
-         end
-         (- end (middle beg end)))
-       (next code)))))
-
-(defn pass-id
-   [pass]
-   (let [[row col] (parse-pass pass)]
-     (+ (* (decode row 127 \F \B) 8)
-        (decode col 7 \L \R))))
+(defn seat-id [pass]
+  (let [[row col] (partition-all 7 pass)]
+    (+ (chars->num col)
+       (* 8 (chars->num row)))))
 
 ;; part 1
-(apply max (map pass-id input))
+(apply max (map seat-id input))
 ;; => 938
 
 ;; part 2
-(apply min (map pass-id input))
+(apply min (map seat-id input))
 
-(def real-passes (map pass-id input))
+(def real-passes (map seat-id input))
 
 (def complete-passes
-  (range (apply min (map pass-id input))
-         (apply max (map pass-id input))))
+  (range (apply min (map seat-id input))
+         (apply max (map seat-id input))))
 
 (set/difference (set complete-passes)
                 (set real-passes))
 ;; => #{696}
-
-
-
